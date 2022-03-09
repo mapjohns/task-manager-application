2ae6f5d0fa078570fec96a37dd9fa1efe02b3746
Matthew <mattj20@gmail.com>
2022-03-08 20:16:53 -0800

    User Controller, added creation routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
 
     def index
+        @users = User.all
+        @counter = 1
+    end
+
+    def new
+        @user = User.new()
+    end
+
+    def create
+        params.inspect
+        @user = User.create(user_params)
+        if @user.valid?
+            redirect_to '/users'
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password)
 
     end
 
