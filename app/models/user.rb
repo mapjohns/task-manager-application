16bc6d9fccce6042a7c23b51c82ef09bcf3db1fc
Matthew <mattj20@gmail.com>
2022-03-08 20:17:16 -0800

    Added model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    
+    validates_presence_of :first_name, :last_name, :username, :password, :password_confirmation
+    validates :username, uniqueness: true
 
 end
