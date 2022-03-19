aed829691e1b5b1db2a3203242d3f1aff980a277
Matthew <mattj20@gmail.com>
2022-03-19 16:48:43 -0700

    Models added/updated

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ActiveRecord::Base
-
+    has_many :tasks
+    has_many :users, through: :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
+    has_many :tasks
+    has_many :places, through: :tasks
     validates_presence_of :first_name, :last_name, :username, :password, :password_confirmation
     validates :username, uniqueness: true
 
