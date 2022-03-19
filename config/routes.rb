793fde3dcf9ed0dc1ed63e0b56db9acd2ef2a598
Matthew <mattj20@gmail.com>
2022-03-19 16:50:34 -0700

    Session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+
+  get '/signin', to: 'session#new'
+  post '/signin', to: 'session#create'
+  delete '/logout', to: 'session#destroy', as: :logmeout
 end
