74e896bf1c2d5255a714827c2b4d2872225e521d
Matthew <mattj20@gmail.com>
2022-03-19 16:47:53 -0700

    Create and params added to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,18 @@
 class TasksController < ApplicationController
 
+    def new
+        @task = Task.new()
+    end
+
+    def create
+        binding.pry
+        @task = Task.create(task_params)
+    end
+
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description, :status)
+    end
 end
